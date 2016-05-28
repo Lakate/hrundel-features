e04f1b5150743a3e6dac51ff1ac8b09d675a12ab
Латыпова Екатерина <katelat3009@gmail.com>
2016-05-28 20:59:42 +0500

    temp

--- scripts/gitHubAuth.js
+++ scripts/gitHubAuth.js
@@ -2,9 +2,9 @@
 
 module.exports.auth = github => {
     github.authenticate({
-        type: "basic",
-        username: "lakate",
-        password: "87051021004Aa"
+        type: 'basic',
+        username: 'lakate',
+        password: '87051021004Aa'
     });
     // const token = process.env.TOKEN;
     // github.authenticate({
