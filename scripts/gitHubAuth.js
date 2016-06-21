3154c7696b79d9eb7b1e6204c59ba37a2ac876ac
Латыпова Екатерина <katelat3009@gmail.com>
2016-06-21 18:05:33 +0500

    обновление конфигурационного файла

--- scripts/gitHubAuth.js
+++ scripts/gitHubAuth.js
@@ -1,9 +1,17 @@
 'use strict';
 
-module.exports.auth = github => {
-    const token = process.env.TOKEN;
-    github.authenticate({
-        type: 'token',
-        token
-    });
-};
+const config = require('config');
+const GITHUB_SETTINGS = config.get('github');
+GITHUB_SETTINGS.headers = {'user-agent': 'Honest Hrundel'};
+
+const GitHubApi = require('github');
+
+let github = new GitHubApi(GITHUB_SETTINGS);
+
+const token = process.env.TOKEN;
+github.authenticate({
+    type: 'token',
+    token
+});
+
+module.exports = github;
